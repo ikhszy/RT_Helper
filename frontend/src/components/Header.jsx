79b4b17f0aef4713edca6e0e759986ab6211d7f4
ikhszy <ikhszy@gmail.com>
2024-07-09 15:52:37 +0700

    add footer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,21 @@
 import {Navbar, Nav, Container, NavbarBrand} from 'react-bootstrap';
 import {FaShoppingCart, FaUser } from "react-icons/fa";
-
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
     <header>
         <Navbar
-            bg="dark" variant="dark" expand="md" collapseOnSelect>
+            bg="light" data-bs-theme="dark" expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">shop</Navbar.Brand>
+                <Navbar.Brand href="/">
+                    <img src={logo} alt='shop'>
+                    </img> shop</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <nav className="ms-auto">
-                        <Nav.Link href="/cart"><FaShoppingCart />Cart</Nav.Link>
-                        <Nav.Link href="/login"><FaUser />SignIn</Nav.Link>
+                        <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
+                        <Nav.Link href="/login"><FaUser /> Login</Nav.Link>
                     </nav>
                 </Navbar.Collapse>
             </Container>
